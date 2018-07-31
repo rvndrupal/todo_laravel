be89587621d68c97765a4e6b6fb399063653374f
Rodrigo <rodrigodrupal1@gmail.com>
2018-07-31 18:53:33 -0500

    Terminan las migraciones

--- curso_online/database/migrations/2018_07_31_234458_create_goals_table.php
+++ curso_online/database/migrations/2018_07_31_234458_create_goals_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateReviewsTable extends Migration
+class CreateGoalsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateReviewsTable extends Migration
      */
     public function up()
     {
-        Schema::create('reviews', function (Blueprint $table) {
+        Schema::create('goals', function (Blueprint $table) {
             $table->increments('id');
 	        $table->unsignedInteger('course_id');
 	        $table->foreign('course_id')->references('id')->on('courses');
-	        $table->unsignedInteger('user_id');
-	        $table->foreign('user_id')->references('id')->on('users');
-	        $table->integer('rating');
-	        $table->text('comment')->nullable();
+	        $table->string('goal');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateReviewsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('reviews');
+        Schema::dropIfExists('goals');
     }
 }
