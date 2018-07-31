b25bfd24cf6ac140793c18846bd150dc136fb0ef
Rodrigo <rodrigodrupal1@gmail.com>
2018-07-31 17:31:14 -0500

    Comienzan las migraciones del proyecto

--- curso_online/database/migrations/2014_10_12_000000_create_users_table.php
+++ curso_online/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,52 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->comment('Campo nombre');
+            $table->text('description');
+            $table->timestamps();
+        });
+
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('role_id')->default(\App\Role::STUDENT);
+            $table->foreign('role_id')->references('id')->on('roles');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password');         
+            $table->string('picture')->nullable();
+
+            //cashier columns
+            $table->string('stripe_id')->nullable();
+            $table->string('card_brand')->nullable();
+            $table->string('card_last_four')->nullable();
+            $table->timestamp('trial_ends_at')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
+        });
+
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('name');
+            $table->string('stripe_id');
+            $table->string('stripe_plan');
+            $table->integer('quantity');
+            $table->timestamp('trial_ends_at')->nullable();//cuando finaliza la version de prueba
+            $table->timestamp('ends_at')->nullable();//cuando la subscriptions
+            $table->timestamps();
+        });
+
+        Schema::create('user_social_accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('provider');
+            $table->string('provider_uid');            
+            
         });
     }
 
@@ -31,5 +70,8 @@ class CreateUsersTable extends Migration
     public function down()
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
+        Schema::dropIfExists('subscriptions');
+        Schema::dropIfExists('user_social_accounts');
     }
 }
