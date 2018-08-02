e74d0224207bc278247177fbeadff710f36d8bae
Rodrigo <rodrigodrupal1@gmail.com>
2018-08-02 12:12:32 -0500

    Se carga TODO VUE

---
+++ TODO_VUE/interpolar2.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '.vue',
+    data: {
+        mensaje: 'Aprende Vue.js'
+    },
+    methods: {
+        mostrarMensaje: function() {
+            return 'Aprende Vue facilmente -Message, Esta es la Funcion'
+        }
+    }
+})
