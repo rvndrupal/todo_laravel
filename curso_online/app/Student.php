1882854c405265238a11b5c7a6de50ad22cb8de8
Rodrigo <rodrigodrupal1@gmail.com>
2018-08-02 13:02:29 -0500

    Continuan las relaciones

--- curso_online/app/Student.php
+++ curso_online/app/Student.php
@@ -9,4 +9,8 @@ class Student extends Model
     public function courses(){
         return $this->belongsToMany(Course::class);
     }
+
+    public function user(){
+        return $this->belongsTo(User::class)->select('id','role_id','name','email');
+    }
 }
