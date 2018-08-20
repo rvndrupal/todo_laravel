e18ed12f418307fe68796cc965c8386440291c81
Rodrigo <rodrigodrupal1@gmail.com>
2018-08-20 11:32:40 -0500

    Crud vue Inicia

---
+++ crud_vue/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use Caffeinated\Shinobi\Models\Role;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class, 20)->create();
+
+        Role::create([
+            'name'  => 'Admin',
+            'slug'  => 'admin',
+            'special' => 'all-access'
+        ]);
+
+        App\User::create([
+            'name'=>'rodrigo',
+            'email'=>'rodrigodrupal1@gmail.com',
+            'password'=>bcrypt('rorro13')
+
+        ]);
+    }
+}
